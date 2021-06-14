cce8fa26f1bfcf73df37a466283928b020a5eea3
Dmitry Diakov <d.diakov@list.ru>
2021-06-14 18:52:57 +0500

    done

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+// TODO: write your code here
+import sum from './basic';
+
+import Game, {GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame} from './game';
+
+console.log('worked');
+
+console.log(sum([1, 2]));
+
+loadGame();
+saveGame();
+
+const game = new Game();
+game.start();
